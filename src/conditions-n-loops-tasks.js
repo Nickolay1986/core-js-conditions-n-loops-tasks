0b7a8365e27e1e3b6744373537bac1c53c57488e
Nickolay1986 <kolya-grek@list.ru>
2024-01-26 17:10:38 +0600

    feat: add function isIsoscelesTriangle

--- src/conditions-n-loops-tasks.js
+++ src/conditions-n-loops-tasks.js
@@ -96,8 +96,20 @@ function canQueenCaptureKing(queen, king) {
  *  2, 2, 5   => false
  *  3, 0, 3   => false
  */
-function isIsoscelesTriangle(/* a, b, c */) {
-  throw new Error('Not implemented');
+function isIsoscelesTriangle(a, b, c) {
+  if (a === 0 || b === 0 || c === 0) {
+    return false;
+  }
+  if (a === b && c < a + b) {
+    return true;
+  }
+  if (c === b && a < c + b) {
+    return true;
+  }
+  if (c === a && b < c + a) {
+    return true;
+  }
+  return false;
 }
 
 /**
